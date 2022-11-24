ecff0d01c382ea1a786694f92991b8b620d0502f
fazlarabby1 <fazlarabby41@gmail.com>
2022-11-24 23:38:43 +0600

    connected with mongodb and created get the categories data DB

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -7,6 +9,27 @@ const app = express();
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5i4qdkw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('laptopResaler').collection('categories');
+
+        app.get('/categories', async(req, res)=>{
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
 
 app.get('/', (req, res)=>{
     res.send('Hey, laptop resale is running Yayy');
